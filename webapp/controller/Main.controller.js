41053a711feb8762c00d89828c7c009468e0f20a
Jansen Penido <jansen.penido@gmail.com>
2020-07-04 18:54:19 -0300

    Added best sellers list.

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -10,26 +10,45 @@ sap.ui.define([
 		onInit: function () {
 			this._oComponent = this
 			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass())
+
+			this._getBestSellers()
 		},
 
 		onSearch : function (oEvent) {
 			this.byId("page").setBusy(true)
 
 			const oModel = new sap.ui.model.json.JSONModel()
-			sap.ui.getCore().setModel(oModel, "mBooksAPI")
+			sap.ui.getCore().setModel(oModel, "mBooksSearch")
 
 			let sQuery = oEvent.getParameter("query").toLowerCase().replace(" ", "+")
 			let sCallUrl = this.apiUrl + sQuery + "&maxResults=6"
 
 			oModel.loadData(sCallUrl)
-			oModel.attachRequestCompleted(this._handleRequestCompleted, this)
+			oModel.attachRequestCompleted(this._handleSearchCompleted, this)
 		},
 
-		_handleRequestCompleted : function (oEvent) {
+		_handleSearchCompleted : function (oEvent) {
 			let oData = oEvent.getSource().getData()
-			console.log(oData)
+			this.getView().setModel(new sap.ui.model.json.JSONModel(oData), "mBooksSearch")
 
-			this.getView().setModel(new sap.ui.model.json.JSONModel(oData), "mBooksAPI")
+			this.byId("page").setBusy(false)
+		},
+
+		_getBestSellers : function () {
+			this.byId("page").setBusy(true)
+
+			const oModel = new sap.ui.model.json.JSONModel()
+			sap.ui.getCore().setModel(oModel, "mBooksSearch")
+
+			let sCallUrl = this.apiUrl + "subject:authors&maxResults=9"
+
+			oModel.loadData(sCallUrl)
+			oModel.attachRequestCompleted(this._handleBestSellersCompleted, this)
+		},
+
+		_handleBestSellersCompleted : function (oEvent) {
+			let oData = oEvent.getSource().getData()
+			this.getView().setModel(new sap.ui.model.json.JSONModel(oData), "mBooksBestSellers")
 
 			this.byId("page").setBusy(false)
 		}
